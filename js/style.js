5fd0da1d39607dc461bfd471fd109cb3985fe0c3
manhkhanh03 <khanhmanh0705@gmail.com>
2022-11-29 20:05:30 +0700

    add

--- js/style.js
+++ js/style.js
@@ -1,7 +1,7 @@
 // link
 $(document).ready(function () {
     function activeTab(ojb) {
-        $('.navbar-btn .list-item').removeClass('active')
+        $('.navbar-btn li').removeClass('active')
 
         $(ojb).addClass('active')
 
@@ -12,7 +12,7 @@ $(document).ready(function () {
         $(id).show()
     }
 
-    $('.navbar-btn .list-item').click(function () {
+    $('.navbar-btn li').click(function () {
         activeTab(this)
         return false
     })
@@ -39,6 +39,63 @@ $(document).ready(function () {
     })
 })
 
+
+
+// focus input
+function focusInput(windowWidth) {
+    if (windowWidth <= 520) {
+        console.log('wd:' + windowWidth)
+        $('.search').focusin(function () {
+            $('.focus-input').hide()
+        })
+        $('.search').focusout(function () {
+            $('.focus-input').show()
+        })
+    } else
+        $('.search').focusin(function () {
+            $('.focus-input').show()
+        })
+
+}
+
+// hien thi thanh navbar
+function grow(windowWidth) {
+    if (windowWidth >= 1024)
+        document.getElementById('navbar-btn').style.display = 'flex'
+    else {
+        document.getElementById('navbar-btn').style.display = 'none'
+    }
+}
+
+function onclick(windowWidth) {
+    $(document).ready(function () {
+        if (windowWidth <= 1024) {
+            $('.navbar li').click(function () {
+                document.getElementById('navbar-btn').style.display = 'none'
+            })
+        }
+        else {
+            $('.navbar li').click(function () {
+                document.getElementById('navbar-btn').style.display = 'flex'
+            })
+        }
+    })
+}
+
+let windowWidth = document.getElementById('website').clientWidth
+focusInput(windowWidth)
+grow(windowWidth)
+onclick(windowWidth)
+
+$(document).ready(function () {
+    $(window).resize(function () {
+        windowWidth = document.getElementById('website').clientWidth
+        focusInput(windowWidth)
+        grow(windowWidth)
+        onclick(windowWidth)
+    })
+})
+
 // sticky
 $(document).ready(function () {
     $(window).scroll(function () {
@@ -57,16 +114,19 @@ $(document).ready(function () {
     })
 })
 
+
 //slickSlider
-let selling = document.getElementById('best-selling').clientWidth
+let selling
 
 $(document).ready(function () {
+    selling = document.getElementById('best-selling').clientWidth
+    console.error(selling)
     if (selling >= 1024) {
         $('.autoplay').slick({
             slidesToShow: 5,
             slidesToScroll: 1,
             autoplay: true,
-            autoplaySpeed: 2000
+            autoplaySpeed: 2000,
         });
     }
     else if (selling >= 920) {
@@ -78,6 +138,7 @@ $(document).ready(function () {
         });
     }
     else if (selling >= 400) {
+        console.log(selling)
         $('.autoplay').slick({
             slidesToShow: 3,
             slidesToScroll: 1,
@@ -86,6 +147,7 @@ $(document).ready(function () {
         });
     }
     else {
+        console.warn(selling)
         $('.autoplay').slick({
             slidesToShow: 2,
             slidesToScroll: 1,
@@ -129,65 +191,32 @@ function unNavHidden() {
 unHidden();
 unNavHidden();
 
-$(document).ready(function () {
-    let navbar = document.getElementById('website').clientWidth
-    if (navbar <= 1024) {
-        $(window).resize(function () {
-            console.log(navbar)
-            $('.navbar-btn li').click(function () {
-                $('.navbar-btn').hide()
-            })
-        })
-    }
-    else {
-        $(window).resize(function () {
-            console.log(navbar)
-            document.getElementById('navbar-btn').style.display = 'flex'
-        })
-    }
-})
 
-
-
-// bộ lọc
+// Bo loc
+// Ẩn bộ lọc
 function hiddenFilter() {
     let hidden = document.getElementById('hidden')
-    let result = document.getElementById('result-filter')
+    let resultFilter = document.getElementById('result-filter')
     let hiddenFilter = document.getElementById('hidden-filter')
-    let rightFilter = document.getElementById('right-filter')
     let leftFilter = document.getElementById('left-filter')
     let x = document.getElementById('x')
 
     if (x.value == '0') {
         hidden.style.display = 'unset';
-        result.style.width = '60%';
+        resultFilter.style.width = '60%';
         hiddenFilter.style.maxWidth = '20%';
         hiddenFilter.style.minWidth = '255.82px';
-        rightFilter.style.width = '78%';
         leftFilter.style.display = 'block';
         x.value = '1';
     } else {
         hidden.style.display = 'none';
-        result.style.width = '72%';
+        resultFilter.style.width = '72%';
         hiddenFilter.style.maxWidth = '2%';
         hiddenFilter.style.minWidth = '40px';
-        rightFilter.style.width = '100%';
         leftFilter.style.display = 'none';
         x.value = '0';
     }
 };
-
-$(document).ready(function () {
-    $('.barsButton').click(function () {
-        $('.left-filter').slideToggle()
-    })
-})
-
-$(document).ready(function () {
-    $('.close-filter').click(function () {
-        $('.left-filter').hide()
-    })
-})
 
 // Sắp xếp bộ lọc
 function sortFilter() {
@@ -245,79 +274,12 @@ plusThree.addEventListener('click', function (event) {
     minusThree.style.display = 'block'
 })
 
-// Phan trang
-let widthPage = document.getElementById('website').clientWidth
-
-let limit;
-if (widthPage >= 740 && widthPage <= 1024)
-    limit = 12;
-else
-    limit = 10;
-
-let thisPage = 1;
-let list = document.querySelectorAll('.product-product .box-product');
-
-function loadItem() {
-    let beginGet = limit * (thisPage - 1);
-    let endGet = limit * thisPage - 1;
-    list.forEach((item, key) => {
-        if (key >= beginGet && key <= endGet)
-            item.style.display = 'block';
-        else
-            item.style.display = 'none';
-    })
-    listPage();
-}
-loadItem();
-
-function listPage() {
-    let count = Math.ceil(list.length / limit);
-    document.querySelector('.listPage').innerHTML = '';
-
-    // prev
-    if (thisPage >= 1) {
-        let prev = document.createElement('li');
-        prev.classList.add('fa-solid', 'fa-chevron-left');
-        if (thisPage > 1) {
-            prev.setAttribute('onclick', 'changePage(' + (thisPage - 1) + ')');
-            document.querySelector('.listPage').appendChild(prev);
-        }
-        else {
-            document.querySelector('.listPage').appendChild(prev);
-        }
-    }
-
-    for (i = 1; i <= count; i++) {
-        let newPage = document.createElement('li');
-        newPage.innerText = i;
-        if (i == thisPage)
-            newPage.classList.add('active');
-        newPage.setAttribute('onclick', 'changePage(' + i + ')');
-        document.querySelector('.listPage').appendChild(newPage);
-    }
-
-    // next
-    if (thisPage <= count) {
-        let next = document.createElement('li');
-        next.classList.add('fa-solid', 'fa-chevron-right');
-        if (thisPage < count) {
-            next.setAttribute('onclick', 'changePage(' + (thisPage + 1) + ')');
-            document.querySelector('.listPage').appendChild(next);
-        }
-        else
-            document.querySelector('.listPage').appendChild(next);
-    }
-}
-
-function changePage(i) {
-    thisPage = i;
-    loadItem();
-}
-
 //heart
-let like = document.getElementsByClassName('btn-like')
-for (i = 0; i < like.length; i++) {
-    like[i].addEventListener('click', function (event) {
-        event.currentTarget.classList.toggle('pink');
-    })
-}
+// let likeButton = document.getElementsByClassName('btn-like')
+
+// for (i = 0; i < likeButton.length; i++) {
+//     likeButton[i].addEventListener('click', function (event) {
+//         event.currentTarget.classList.toggle('pink');
+//     })
+// }
+
